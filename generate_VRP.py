8d943b98ac5eecd4e99c160e33c289534f276e7a
swvandekamp <stevenvandekamp@gmail.com>
2024-12-20 15:31:30 +0100

    Generate testcases

--- generate_VRP.py
+++ generate_VRP.py
@@ -3,8 +3,8 @@ import numpy as np
 import itertools
 
 # Parameters
-num_jobs = 100  # Number of jobs (N)
-num_machines = 5  # Number of machines (M)
+num_jobs = 3  # Number of jobs (N)
+num_machines = 1  # Number of machines (M)
 seed = 69  # Random seed for reproducibility
 np.random.seed(seed)
 
@@ -19,8 +19,8 @@ N_a = N.union(N_s).union(N_z)
 ## Generate random values for r (travel time) and d (travel cost) ##
 ####################################################################
 
-# Assign random 2D coordinates to each node
-coordinates = {i: (np.random.uniform(0, 100), np.random.uniform(0, 100)) for i in N_a}
+# Assign random 2D integer coordinates to each node
+coordinates = {i: (np.random.randint(0, 101), np.random.randint(0, 101)) for i in N_a}
 
 # Calculate travel times (Euclidean distances) for i < j
 r = {}
@@ -28,7 +28,7 @@ for i, j in itertools.combinations(N_a, 2):
     x1, y1 = coordinates[i]
     x2, y2 = coordinates[j]
     distance = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    r[(i, j)] = distance
+    r[(i, j)] = round(distance)
 
 # Define travel cost as a scaled function of travel time
 # A random cost factor is applied to each travel time
@@ -36,7 +36,7 @@ cost_factors = {i: np.random.uniform(0.5, 1.5) for i in N_a}  # Random scaling f
 d = {}
 for (i, j), travel_time in r.items():
     scaling_factor = (cost_factors[i] + cost_factors[j]) / 2  # Average scaling factor
-    d[(i, j)] = travel_time * scaling_factor
+    d[(i, j)] = round(travel_time * scaling_factor)
 
 ###########################################################################
 ## Generate random values for a (work duration) and c (cost coefficient) ##
@@ -66,9 +66,10 @@ MachineProperties = {
     "Start Depot": [f"s{machine}" for machine in M],
     "End Depot": [f"z{machine}" for machine in M],
 }
-with pd.ExcelWriter("generated_test_case.xlsx") as writer:
+file_name = f"generated_test_case_{num_jobs}.xlsx"
+with pd.ExcelWriter(file_name) as writer:
     pd.DataFrame(NodeConnections).to_excel(writer, sheet_name="Node Connections", index=False)
     pd.DataFrame(NodeProperties).to_excel(writer, sheet_name="Node Properties", index=False)
     pd.DataFrame(MachineProperties).to_excel(writer, sheet_name="Machine Properties", index=False)
 
-print("File 'generated_test_case.xlsx' created.")
+print(f"File '{file_name}' created.")
